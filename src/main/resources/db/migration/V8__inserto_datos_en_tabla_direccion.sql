da7beed87fdf9923929635b6f311c6c0b4091117
RaiMorales <raimundo.niwe@gmail.com>
2026-04-29 14:42:20 +0200

    Creo un script DML para insertar datos en la tabla direccion

---
+++ src/main/resources/db/migration/V8__inserto_datos_en_tabla_direccion.sql
@@ -0,0 +1,7 @@
+INSERT INTO 
+    `direccion`
+    (`nombre_calle`, `numero_calle`, `usuario_id`, `direccion_principal`)
+VALUES
+    ('Calle Mayor', 123, 1, 0),
+    ('Avenida de la Constitución', 45, 1, 1),
+    ('Calle del Sol', 67, 1, 0);

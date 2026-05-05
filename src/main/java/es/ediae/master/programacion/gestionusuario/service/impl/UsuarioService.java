8b4953acb2038a5e05a57bc4f13578d28499e0fa
RaiMorales <raimundo.niwe@gmail.com>
2026-05-05 17:45:37 +0200

    Creo la clase UsuarioService en la carpeta impl de service

---
+++ src/main/java/es/ediae/master/programacion/gestionusuario/service/impl/UsuarioService.java
@@ -0,0 +1,71 @@
+package es.ediae.master.programacion.gestionusuario.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import es.ediae.master.programacion.gestionusuario.entity.UsuarioEntity;
+import es.ediae.master.programacion.gestionusuario.repository.UsuarioRepository;
+import es.ediae.master.programacion.gestionusuario.service.IUsuarioService;
+import es.ediae.master.programacion.gestionusuario.service.UsuarioModel;
+
+@Service
+public class UsuarioService implements IUsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    
+    @Override
+    public List <UsuarioModel> obtenerUsuarios() {
+        
+        return usuarioRepository.findAll().stream()
+                .map(UsuarioModel::fromEntity)
+                .toList();
+    }
+
+    @Override
+    public UsuarioModel obtenerUsuarioPorId(Integer idUsuario) {
+        
+        return usuarioRepository.findById(idUsuario)
+            .map(UsuarioModel::fromEntity)
+            .orElse(null);
+    }
+
+    @Override
+    public UsuarioModel crearUsuario(UsuarioEntity usuario) {
+        
+        return UsuarioModel.fromEntity(usuario);
+    }
+
+    @Override
+    public UsuarioModel actualizarUsuarioPorId(Integer idUsuario, UsuarioModel usuarioModel) {
+        
+        Optional <UsuarioEntity> optionalUsuario = usuarioRepository.findById(idUsuario);
+        if (optionalUsuario.isPresent()) {
+
+            UsuarioEntity usuarioEntity = optionalUsuario.get();
+            usuarioEntity.setNickUsuario(usuarioModel.getNickUsuario());
+            usuarioEntity.setContrasena(usuarioModel.getContrasena());
+            usuarioEntity.setGenero(usuarioModel.getGenero());
+            usuarioEntity.setNombre(usuarioModel.getNombre());
+            usuarioEntity.setPrimerApellido(usuarioModel.getPrimerApellido());
+            usuarioEntity.setSegundoApellido(usuarioModel.getSegundoApellido());
+            usuarioEntity.setFechaNacimiento(usuarioModel.getFechaNacimiento());
+            usuarioEntity.setHoraDesayuno(usuarioModel.getHoraDesayuno());
+            usuarioEntity.setPuestoTrabajo(usuarioModel.getPuestoTrabajo());
+            return UsuarioModel.fromEntity(usuarioRepository.save(usuarioEntity));
+        }
+        else {
+            return null;
+        }
+    }
+
+    @Override
+    public void eliminarUsuarioPorId(Integer idUsuario) {
+        
+        usuarioRepository.deleteById(idUsuario); 
+    }
+
+}
